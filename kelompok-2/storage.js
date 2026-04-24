378e3bab1519de767842db5364f085887c57ceaf
tinsarirauhana <tinsari.r@mhs.usk.ac.id>
2026-04-24 19:42:30 +0700

    progress hari ini menambahkan storage.js

--- kelompok-2/storage.js
+++ kelompok-2/storage.js
@@ -1,1 +1,7 @@
-console.log("storage ready");
+// menyimpan data marker sementara (masih basic)
+let markers = [];
+
+function addData(lat, lng) {
+  markers.push({ lat, lng });
+  console.log("Data masuk:", markers);
+}
