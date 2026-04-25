4bae9b23eaafc01136be4b6a391543e9aab84677
tinsarirauhana <tinsari.r@mhs.usk.ac.id>
2026-04-25 18:45:12 +0700

    menambahkan 15 data koordinat masjid Banda Aceh dan Aceh Besar

---
+++ kelompok-2/markers-data.js
@@ -0,0 +1,28 @@
+// markers-data.js
+// Data koordinat masjid di Banda Aceh & Aceh Besar
+// Kontributor: Tinsari (Data & Testing)
+
+const markersData = [
+  // === HARI 1 (kompensasi) ===
+  { id: 1, nama: "Masjid Raya Baiturrahman", lat: 5.5502, lng: 95.3182, kota: "Banda Aceh" },
+  { id: 2, nama: "Masjid Jamik Luengbata", lat: 5.5371, lng: 95.3456, kota: "Banda Aceh" },
+  { id: 3, nama: "Masjid Al-Furqan Beurawe", lat: 5.5456, lng: 95.3367, kota: "Banda Aceh" },
+  { id: 4, nama: "Masjid Darul Aman Lamprit", lat: 5.5523, lng: 95.3301, kota: "Banda Aceh" },
+  { id: 5, nama: "Masjid Al-Ikhlas Kuta Alam", lat: 5.5601, lng: 95.3289, kota: "Banda Aceh" },
+
+  // === HARI 2 (kompensasi) ===
+  { id: 6, nama: "Masjid Baitussalihin Ulee Lheue", lat: 5.5701, lng: 95.2901, kota: "Banda Aceh" },
+  { id: 7, nama: "Masjid Agung Aceh Besar", lat: 5.5214, lng: 95.3789, kota: "Aceh Besar" },
+  { id: 8, nama: "Masjid Nurul Islam Darussalam", lat: 5.4983, lng: 95.3821, kota: "Aceh Besar" },
+  { id: 9, nama: "Masjid Al-Munawwarah Peukan Bada", lat: 5.5012, lng: 95.2634, kota: "Aceh Besar" },
+  { id: 10, nama: "Masjid Darul Falah Lhoknga", lat: 5.4756, lng: 95.2312, kota: "Aceh Besar" },
+
+  // === HARI 3 ===
+  { id: 11, nama: "Masjid Ulee Kareng", lat: 5.5389, lng: 95.3012, kota: "Banda Aceh" },
+  { id: 12, nama: "Masjid Al-Makmur Lambaro Skep", lat: 5.5278, lng: 95.3423, kota: "Banda Aceh" },
+  { id: 13, nama: "Masjid Kopelma Darussalam", lat: 5.5567, lng: 95.3712, kota: "Banda Aceh" },
+  { id: 14, nama: "Masjid Baitul Musyahadah Ingin Jaya", lat: 5.5134, lng: 95.4023, kota: "Aceh Besar" },
+  { id: 15, nama: "Masjid Al-Huda Kuta Malaka", lat: 5.4612, lng: 95.4234, kota: "Aceh Besar" },
+];
+
+console.log("Total marker data loaded:", markersData.length);
