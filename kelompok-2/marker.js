b78dd68e0faa4341091a43a10701fcf7bd12d572
khalishaadzra <khalishaadzraini@gmail.com>
2026-04-25 23:23:07 +0700

    Add reset marker functionality

--- kelompok-2/marker.js
+++ kelompok-2/marker.js
@@ -50,3 +50,15 @@ function addMarkerToSidebar(lat, lng) {
   mosqueList.appendChild(item);
 }
 
+document.getElementById("resetBtn").addEventListener("click", function () {
+  markers.forEach((marker) => {
+    map.removeLayer(marker);
+  });
+
+  markers = [];
+
+  document.querySelector(".mosque-list").innerHTML = "";
+
+  updateMarkerCount();
+});
+
